01c7ecaa5bde7149a6575877c0cd93e310c1c995
lienh <lienh@192.168.0.166>
2026-02-20 10:02:05 +0100

    Naar apart bestand verplaatst methodes in interval-klasse

--- intervallen/src/intervallen/intervallenTest.java
+++ intervallen/src/intervallen/intervallenTest.java
@@ -5,21 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class intervallenTest {
-
-	class Interval {
-		// REPRESENTATIE
-		int ondergrens; 
-		int lengte; 
-	}
-	
 	
 	@Test
 	void test() {
 		Interval myInterval = new Interval() ;
 		Interval.setOndergrens(myInterval, 3); 
 		Interval.setBovengrens(myInterval, 7); 
-		int lengte = Interval.getBovengrens(myInterval) - interval.getOndergrens(myInterval);
-		assertEquals(4, lengte);
+		int lengte = Interval.getBovengrens(myInterval) - Interval.getOndergrens(myInterval);
+		assertEquals(4,lengte); 
 	}
 
 }
